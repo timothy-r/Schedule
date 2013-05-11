2765ffca8cf6b59c60dc8d0c024b278dadaa356f
timothy-r <tim.rodger@gmail.com>
2013-05-11 10:13:09 +0100

    don't put class constructors into interfaces

--- lib/Ace/Schedule/iMatcher.php
+++ lib/Ace/Schedule/iMatcher.php
@@ -4,6 +4,9 @@ use Ace\Schedule\iValue;
 use DateTime;
 
 interface iMatcher {
+    /**
+    * @todo remove this from interface
+    */
 	public function __construct(iValue $value);
 	public function matches(DateTime $date_time);
 }
